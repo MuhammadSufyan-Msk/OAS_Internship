e95de8b650e991117e1e299138a4cf34b6de05b7
MuhammadSufyan-MSK <msufiyan2003@gmail.com>
2026-04-09 11:44:19 +0500

    adding loading spinner

--- Material Grid With API/material-grid-app/src/app/app.ts
+++ Material Grid With API/material-grid-app/src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // Check this import path carefully based on your folder structure!
-import { GridComponent } from './components/data-grid/data-grid'; 
+import { DataGridComponent } from './components/data-grid/data-grid'; 
 
 @Component({
   selector: 'app-root',
   standalone: true, // Make sure this is present
   imports: [
     RouterOutlet, 
-    GridComponent // THIS CLEARS THE ERROR
+    DataGridComponent // THIS CLEARS THE ERROR
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
--- Material Grid With API/material-grid-app/src/app/components/data-grid/data-grid.ts
+++ Material Grid With API/material-grid-app/src/app/components/data-grid/data-grid.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, ViewChild, inject, signal } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; // Import this!
+import { CommonModule } from '@angular/common'; // Needed for @if or *ngIf
 import { Data } from '../../services/data';
 
 @Component({
-  selector: 'app-grid',
+  selector: 'app-data-grid',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule],
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatProgressSpinnerModule],
   templateUrl: './data-grid.html',
   styleUrl: './data-grid.scss'
 })
-export class GridComponent implements OnInit {
+export class DataGridComponent implements OnInit {
   private dataService = inject(Data);
+  
+  // Create a loading signal
+  isLoading = signal(true); 
   
   displayedColumns: string[] = ['id', 'name', 'email'];
   dataSource = new MatTableDataSource<any>([]);
@@ -19,8 +24,13 @@ export class GridComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   async ngOnInit() {
-    const data = await this.dataService.getGridData();
-    this.dataSource.data = data;
-    this.dataSource.paginator = this.paginator;
+    try {
+      this.isLoading.set(true); // Start loading
+      const data = await this.dataService.getGridData();
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+    } finally {
+      this.isLoading.set(false); // Stop loading regardless of success/fail
+    }
   }
 }
